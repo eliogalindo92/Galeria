150734fa5196bb4c385a08fe9a13eb01ce86be21
Elio Galindo Fonseca <eliogalindo92@yahoo.com>
2022-03-25 15:40:10 -0400

    Nuevos cambios

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -1,12 +1,9 @@
 // JavaScript Document
-function cant_elem(){
-	for(var i=0; i<4; i++){
-	var elementos = document.querySelectorAll(".menu");
-	elementos[i].onclick=mensaje;
-}
-}
 function mensaje(){
 	alert("Mensaje");
 }
 
-window.onload=cant_elem;
+if (document.getElementsByClassName("menu").onclick==true){
+	mensaje();
+}
+
